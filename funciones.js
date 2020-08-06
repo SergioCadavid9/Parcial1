4d7784ab13dc4461dd2c4a12731388e3e9ecf6ae
SergioCadavid97 <sergiocadavid9@gmail.com>
2020-08-06 16:50:11 -0500

    Primer parcial

---
+++ funciones.js
@@ -0,0 +1,42 @@
+function boton7() {
+    mostrar.value = "7";
+}
+
+function boton8() {
+    mostrar.value = "8";
+}
+function boton9() {
+    mostrar.value = "9";
+}
+function boton4() {
+    mostrar.value = "4";
+}
+function boton5() {
+    mostrar.value = "5";
+}
+function boton6() {
+    mostrar.value = "6";
+}
+function boton1() {
+    mostrar.value = "1";
+}
+function boton2() {
+    mostrar.value = "2";
+}
+function boton3() {
+    mostrar.value = "3";
+}
+
+function suma() {
+ var boton1 = parseInt(boton1.value);
+ var boton2 = parseInt(boton2.value);
+suma= boton1 + boton2;
+mostrar.value = suma;
+
+}
+function codi(){
+    mostrar.value = "1620080563"
+}
+
+
+
